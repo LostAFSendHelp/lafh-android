648050a6eb1641d67476903894f8d87903f62b14
NC Black <chuong315198@Gmail.com>
2021-03-12 00:03:19 +0700

    Basic setup

---
+++ app/src/main/java/com/laf/lafh/views/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.laf.lafh.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.laf.lafh.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
